05b3ab12a1d17d516656105c592bf0dd6d949b16
Harsh Mehta <113175089+HarshMehta2522@users.noreply.github.com>
2023-09-14 00:25:17 +0530

    Update apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const loginCall = async (userCredential, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("http://8800/auth/login", userCredential);
+    const res = await axios.post("/login", userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     console.log(err)
