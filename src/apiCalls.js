34c0e362f34eea5854090d3a7bf11b3f2027c3b1
HarshMehta2522 <maheta.hrs2522@gmail.com>
2023-09-19 16:13:30 +0530

    proxy remove fix

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const loginCall = async (userCredential, dispatch) => {
+  const BACKEND= process.env.REACT_APP_BACKEND_URL;
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("/auth/login", userCredential);
+    const res = await axios.post(BACKEND+"/auth/login", userCredential);
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
     console.log(err)
